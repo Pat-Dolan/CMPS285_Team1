a7321fb492b46688d58346f41c49da4d37fbdbda
Jessica Gaboury <jessgaboury@gmail.com>
2015-10-25 17:02:26 -0500

    Login_controller
    Updates

--- CMPS1/API/application/controllers/login_controller.php
+++ CMPS1/API/application/controllers/login_controller.php
@@ -4,10 +4,9 @@ class Login_controller extends CI_Controller
 
     function validate(){
 //       when posting  json data to the api
-        $json = key($this->input->post(NULL, TRUE));
-        $json = json_decode($json);
-        $username = $json->username;
-        $password = md5($json->password);
+        $json = json_decode(trim(file_get_contents('php://input')), true);
+       $username = $json['username'];
+        $password = md5($json['password']);
 //        when posting with a normal post method with parameters
 //        $username = $this->input->post('username');
 //       $password = md5($this->input->post('password'));

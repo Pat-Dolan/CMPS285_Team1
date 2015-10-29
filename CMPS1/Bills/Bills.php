a87fca260b2ba731185ec0bc38a928761fbef59f
jennamchugh <Jenna.Mchugh@selu.edu>
2015-10-28 22:41:53 -0500

    working bills page
    make sure you guys erase the :8080 after localhost in app.js! Also you
    will need to import the bills.sql file to get the table.

---
+++ CMPS1/Bills/Bills.php
@@ -0,0 +1,34 @@
+<html>
+<head>
+    <title>Bills</title>
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+    <link href="/CMPS1/css/style.css" rel="stylesheet" />
+    <link rel="stylesheet" href="/CMPS1/css/bills.css">
+
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edges us the class early. */
+        .ng-hide {
+            display: none !important;, chrome=1" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no" />
+
+</head>
+<body>
+<script src="/CMPS1/js/geturl.js"></script>
+
+
+<div id="head"></div>
+<!DOCTYPE html>
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+</head>
+<body>
+<p>
+    <?php include("list_files.php"); ?>
+</p>
+</body>
+<footer id="footer"></footer>
+</body>
+</html>

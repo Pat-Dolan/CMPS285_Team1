57c471eb6799c368f9e01a2c9b48405c914dc5bb
siddhanta10 <siddhanta.shrestha@selu.edu>
2015-11-22 20:23:18 -0600

    forgot to commit the controller

--- CMPS1/login/login.controller.js
+++ CMPS1/login/login.controller.js
@@ -18,14 +18,15 @@
 
         function login() {
             vm.dataLoading = true;
+            vm.passright=false;
             AuthenticationService.Login(vm.username, vm.password, function (response) {
-
                 if (response.success) {
                     AuthenticationService.SetCredentials(vm.username, vm.password);
                     $location.path('/');
                 } else {
                     FlashService.Error(response.message);
                     vm.dataLoading = false;
+                    vm.passright=true;
                 }
             });
         };
